386417e3b481b5c4bbc017136024db30d919c5bf
BrentRevelle <revellebrent@icloud.com>
2025-04-03 19:03:13 -0500

    update - seteventlisteners

--- components/todo.js
+++ components/todo.js
@@ -4,22 +4,47 @@ class Todo {
     this._templateElement = document.querySelector(selector);
   }
 
+  _setEventListeners() {
+    // Add event listeners to the checkbox and delete button.
+    //set `change` event listener to the checkbox el
+    //when clicked, change completioin from true to false and vice versa
+    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = todoCheckboxEl.checked;
+      const todoLabel = this._todoElement.querySelector(".todo__label");
+      todoLabel.textContent = this._data.completed
+        ? "Completed"
+        : "Not completed";
+      this._generateCheckedboxEl(); // Update the checkbox element
+    });
+
+    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  _generateCheckedboxEl() {
+    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    const todoLabel = this._todoElement.querySelector(".todo__label");
+    todoCheckboxEl.checked = this._data.completed;
+    todoCheckboxEl.id = `todo-${this._data.id}`;
+    todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
   getView() {
     this._todoElement = this._templateElement.content // this. instead of const makes it able to use outside the function
       .querySelector(".todo") //pass config object to the constructor
       .cloneNode(true);
 
     const todoNameEl = this._todoElement.querySelector(".todo__name");
-    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-    const todoLabel = this._todoElement.querySelector(".todo__label");
     const todoDate = this._todoElement.querySelector(".todo__date");
     const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
     todoNameEl.textContent = this._data.name;
-    todoCheckboxEl.checked = this._data.completed;
 
-    todoCheckboxEl.id = `todo-${this._data.id}`;
-    todoLabel.setAttribute("for", `todo-${this._data.id}`);
+    this._generateCheckedboxEl();
+    this._setEventListeners();
 
     return this._todoElement;
   }
