f16d3003d91147c1998e3d591f4079d8e62197fa
BrentRevelle <revellebrent@icloud.com>
2025-04-06 13:42:17 -0500

    resetValidation and toggleButtonState

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-console.log(uuidv4()); // This will log a new UUID each time the page is loaded.
 
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
+import FormValidator from "../components/FormValidator.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -24,39 +24,6 @@ const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
   return todoElement;
-
-  // to be removed after refactoring                               //done
-  // const todoElement = todoTemplate.content
-  //   .querySelector(".todo")
-  //   .cloneNode(true);
-  // const todoNameEl = todoElement.querySelector(".todo__name");
-  // const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-  // const todoLabel = todoElement.querySelector(".todo__label");
-  // const todoDate = todoElement.querySelector(".todo__date");
-  // const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
-
-  // todoNameEl.textContent = data.name;
-  // todoCheckboxEl.checked = data.completed;
-
-  // // Apply id and for attributes.
-  // // The id will initially be undefined for new todos.
-  // todoCheckboxEl.id = `todo-${data.id}`;
-  // todoLabel.setAttribute("for", `todo-${data.id}`);
-
-  // // If a due date has been set, parsing this it with `new Date` will return a
-  // // number. If so, we display a string version of the due date in the todo.
-  // const dueDate = new Date(data.date);
-  // if (!isNaN(dueDate)) {
-  //   todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
-  //     year: "numeric",
-  //     month: "short",
-  //     day: "numeric",
-  //   })}`;
-  // }
-
-  // todoDeleteBtn.addEventListener("click", () => {
-  //   todoElement.remove();
-  // });
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -81,9 +48,14 @@ addTodoForm.addEventListener("submit", (evt) => {
   const todo = generateTodo(values);
   todosList.append(todo);
   closeModal(addTodoPopup);
+  addTodoForm.reset(); // Reset the form fields after submission
+  newTodoValidator.resetValidation(); // Reset validation state after submission
 });
 
 initialTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
+
+const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
+newTodoValidator.enableValidation(); // Enable validation for the form
